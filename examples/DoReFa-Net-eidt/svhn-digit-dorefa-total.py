47b6ab301e474952086761137e6fe0408cd806e9
tianzhenyu <18215595124@163.com>
2019-07-23 21:15:04 +0800

    1

--- examples/DoReFa-Net-eidt/svhn-digit-dorefa-total.py
+++ examples/DoReFa-Net-eidt/svhn-digit-dorefa-total.py
@@ -109,9 +109,11 @@ class Model(ModelDesc):
                       .FullyConnected('fc1', 10)())
 
         tf.nn.softmax(logits, name='output')
+        '''
         conv_out = tf.get_default_graph().get_tensor_by_name("conv1/output:0")[0]
         fg_out = tf.get_default_graph().get_tensor_by_name("fg1/output:0")[0]
         grad = tf.identity(tf.gradients(fg_out,conv_out),name = 'grad')
+        '''
         # compute the number of failed samples
         wrong = tf.cast(tf.logical_not(tf.nn.in_top_k(logits, label, 1)), tf.float32, name='wrong-top1')
         # monitor training error
