8a6e8e8566fdeb098761c94e3129eaec09200a58
tianzhenyu <18215595124@163.com>
2019-07-24 21:09:23 +0800

    Update examples/DoReFa-Net-eidt/svhn-digit-dorefa-total.py

--- examples/DoReFa-Net-eidt/svhn-digit-dorefa-total.py
+++ examples/DoReFa-Net-eidt/svhn-digit-dorefa-total.py
@@ -119,7 +119,7 @@ class Model(ModelDesc):
         if conv_out is not None  and conv_in is not None and fg_out[0] is not None :
 
             grad_conv = tf.identity(tf.gradients(conv_out,conv_in),name='grad_conv')
-            grad_fg = tf.identity(tf.gradients(fg_out,conv_out),name = 'grad_fg')
+            #grad_fg = tf.identity(tf.gradients(fg_out,conv_out),name = 'grad_fg')
     
         # compute the number of failed samples
         wrong = tf.cast(tf.logical_not(tf.nn.in_top_k(logits, label, 1)), tf.float32, name='wrong-top1')
@@ -174,7 +174,7 @@ def get_config():
             ModelSaver(),
             InferenceRunner(data_test,
                             [ScalarStats('cost'), ClassificationError('wrong-top1')]),
-            DumpTensors(['fg1/MoveMean:0','fg1/MoveVar','conv1/output:0','fg1/output:0','fg1/bn_z:0','grad_conv:0','grad_fg:0'])
+            #DumpTensors(['fg1/MoveMean:0','fg1/MoveVar','conv1/output:0','fg1/output:0','fg1/bn_z:0','grad_conv:0','grad_fg:0'])
         ],
         model=Model(),
         max_epoch=200,
