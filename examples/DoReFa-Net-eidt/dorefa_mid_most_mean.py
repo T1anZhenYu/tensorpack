204c6895243476abae4371cc79472603cd2710d8
tianzhenyu <18215595124@163.com>
2019-07-28 19:02:09 +0800

    bv is not found

--- examples/DoReFa-Net-eidt/dorefa_mid_most_mean.py
+++ examples/DoReFa-Net-eidt/dorefa_mid_most_mean.py
@@ -80,7 +80,7 @@ def get_dorefa(bitW, bitA, bitG):
             if training:
                 print('in training')
 
-                #bm, bv = tf.nn.moments(x, axes=[0,1,2])#calculate batch_mean and batch_var
+                bm, bv = tf.nn.moments(x, axes=[0,1,2])#calculate batch_mean and batch_var
                 mid = tf.nn.top_k (inputs,int(num_chan/2)+1)[0][:,-1]
                 batch_mean = batch_mean.assign(tf.expand_dims(mid,axis=-1))
                 batch_var = batch_var.assign(tf.expand_dims(tf.math.sqrt(bv),axis=-1))
