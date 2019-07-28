821bda48620cf3fe27b50726d4bba9f90c00caa5
tianzhenyu <18215595124@163.com>
2019-07-28 21:21:56 +0800

    mean,most

--- examples/DoReFa-Net-eidt/svhn-digit-dorefa-most-mean.py
+++ examples/DoReFa-Net-eidt/svhn-digit-dorefa-most-mean.py
@@ -13,7 +13,7 @@ from tensorpack.dataflow import dataset
 from tensorpack.tfutils.summary import add_moving_summary, add_param_summary
 from tensorpack.tfutils.varreplace import remap_variables
 
-from dorefa_mid_mean import get_dorefa
+from dorefa_most_mean import get_dorefa
 
 """
 This is a tensorpack script for the SVHN results in paper:
