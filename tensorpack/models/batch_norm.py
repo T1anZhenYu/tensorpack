2aa019cc4897e8d8b39381ca0653b860ca141b2c
tianzhenyu <18215595124@163.com>
2019-07-30 23:10:27 +0800

    BN no beta

--- tensorpack/models/batch_norm.py
+++ tensorpack/models/batch_norm.py
@@ -81,7 +81,7 @@ except ImportError:
     })
 @disable_autograph()
 def BatchNorm(inputs, axis=None, training=None, momentum=0.9, epsilon=1e-5,
-              center=True, scale=True,
+              center=False, scale=False,
               beta_initializer=tf.zeros_initializer(),
               gamma_initializer=tf.ones_initializer(),
               virtual_batch_size=None,
