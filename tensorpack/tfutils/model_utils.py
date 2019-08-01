98fb5ae846e6a4b868744cea583967302504c764
tian <t18215595124@gmail.com>
2019-08-01 10:45:00 +0800

    Update model_utils.py

--- tensorpack/tfutils/model_utils.py
+++ tensorpack/tfutils/model_utils.py
@@ -18,7 +18,6 @@ def describe_trainable_vars():
     Skip variables starting with "tower", as they are just duplicates built by data-parallel logic.
     """
     for ele2 in tf.all_variables():
-        print('in dorefa')
         print(ele2.name)
     train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
     if len(train_vars) == 0:
