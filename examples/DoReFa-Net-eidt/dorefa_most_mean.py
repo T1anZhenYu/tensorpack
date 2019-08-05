f8cc7465b8668ecd75e867fd94498b3504dda9e5
tian <t18215595124@gmail.com>
2019-08-05 15:51:42 +0800

    1

--- examples/DoReFa-Net-eidt/dorefa_most_mean.py
+++ examples/DoReFa-Net-eidt/dorefa_most_mean.py
@@ -81,8 +81,7 @@ def get_dorefa(bitW, bitA, bitG):
 
                 bm, bv = tf.nn.moments(x, axes=[0,1,2])#calculate batch_mean and batch_var
                 mid = tf.nn.top_k (inputs,tf.cast(batch_size0*w*h/2+3,dtype = tf.int32))[0][:,-6:]
-                print('x ',x)
-                print('mid ',mid)
+
                 num = []
                 for i in range(6):
                   label = tf.equal(inputs,tf.expand_dims(mid[:,i],axis=-1))
