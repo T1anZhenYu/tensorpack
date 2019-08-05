f8b544ae7e642d30e1f243c4c68fdd3d6a3c23c2
tian <t18215595124@gmail.com>
2019-08-05 10:51:08 +0800

    Update dorefa_dignoal.py

--- examples/DoReFa-Net-eidt/dorefa_dignoal.py
+++ examples/DoReFa-Net-eidt/dorefa_dignoal.py
@@ -7,7 +7,7 @@ import numpy as np
 def get_std(x,ave):
   return np.sqrt(np.mean(np.square((x-ave)),axis=(0,1,2)))
 def conv_sample_dignoal(kernel_size,x,b,w,h,c):
-    print('b',b)
+    print('b',b.shape)
     if b.shape:
         total = np.zeros(b)
         for ch in range(c):
