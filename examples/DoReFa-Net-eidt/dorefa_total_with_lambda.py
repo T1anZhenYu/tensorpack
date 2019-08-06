4ef262b37ed9c3a862de82ca6e0dfba31b55c13c
tian <t18215595124@gmail.com>
2019-08-06 20:34:12 +0800

    Update dorefa_total_with_lambda.py

--- examples/DoReFa-Net-eidt/dorefa_total_with_lambda.py
+++ examples/DoReFa-Net-eidt/dorefa_total_with_lambda.py
@@ -79,7 +79,7 @@ def get_dorefa(bitW, bitA, bitG):
 
             if training:
                 print('in training')
-
+                bm, bv = tf.nn.moments(x, axes=[0,1,2])
                 batch_mean = batch_mean.assign(tf.expand_dims(bm,axis=-1))
                 batch_var = batch_var.assign(tf.expand_dims(tf.math.sqrt(bv),axis=-1))
 
