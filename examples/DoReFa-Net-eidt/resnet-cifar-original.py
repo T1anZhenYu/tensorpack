48168dac25551c2d471e980318348e2eb0c64db9
tian <t18215595124@gmail.com>
2019-08-12 09:59:08 +0800

    Update resnet-cifar-original.py

--- examples/DoReFa-Net-eidt/resnet-cifar-original.py
+++ examples/DoReFa-Net-eidt/resnet-cifar-original.py
@@ -107,7 +107,7 @@ class Model(ModelDesc):
                       .tf.multiply(49)  # this is due to a bug in our model design
                       .FullyConnected('fct', 10)())
         tf.nn.softmax(logits, name='output')
-        ImageNetModel.compute_loss_and_error(logits, label)
+        return ImageNetModel.compute_loss_and_error(logits, label)
 
 
 def get_inference_augmentor():
