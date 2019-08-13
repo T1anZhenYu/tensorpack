7887c729bf86c15017308f97b9d89b60cb5400bd
tian <t18215595124@gmail.com>
2019-08-13 09:59:49 +0800

    Create resnet-cifar-troditionbn-quantest.py

---
+++ examples/DoReFa-Net-eidt/resnet-cifar-troditionbn-quantest.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: resnet-dorefa.py
+
+import argparse
+import numpy as np
+import os
+import cv2
+import tensorflow as tf
+
+from tensorpack import *
+from tensorpack.dataflow import dataset
+from tensorpack.tfutils.varreplace import remap_variables
+from tensorpack.tfutils.summary import add_moving_summary, add_param_summary
+from dorefa import get_dorefa
+from imagenet_utils import ImageNetModel, eval_classification, fbresnet_augmentor
+from tensorpack.utils.gpu import get_num_gpu
+"""
+This script loads the pre-trained ResNet-18 model with (W,A,G) = (1,4,32)
+It has 59.2% top-1 and 81.5% top-5 validation error on ILSVRC12 validation set.
+
+To run on images:
+    ./resnet-dorefa.py --load ResNet-18-14f.npz --run a.jpg b.jpg
+
+To eval on ILSVRC validation set:
+    ./resnet-dorefa.py --load ResNet-18-14f.npz --eval --data /path/to/ILSVRC
+"""
+
+BITW = 1
+BITA = 4
+BITG = 32
+
+BATCH_SIZE=128
+class Model(ModelDesc):
+    def inputs(self):
+        return [tf.TensorSpec([None, 32, 32, 3], tf.float32, 'input'),
+                tf.TensorSpec([None], tf.int32, 'label')]
+
+    def build_graph(self, image, label):
+        image = image / 256.0
+        is_training = get_current_tower_context().is_training
+        fw, fa, fg = get_dorefa(BITW, BITA, BITG)
+
+        def new_get_variable(v):
+            name = v.op.name
+            # don't binarize first and last layer
+            if not name.endswith('W') or 'conv1' in name or 'fct' in name:
+                return v
+            else:
+                logger.info("Binarizing weight {}".format(v.op.name))
+                return fw(v)
+
+        def nonlin(x):
+            return tf.clip_by_value(x, 0.0, 1.0)
+
+        def activate(x):
+            return fa(nonlin(x))
+
+        def resblock(x, channel, stride):
+            def get_stem_full(x):
+                return (LinearWrap(x)
+                        .Conv2D('c3x3a', channel, 3)
+                        .quan_test_L2norm('stembn')
+                        .apply(activate)
+                        .Conv2D('c3x3b', channel, 3)())
+            channel_mismatch = channel != x.get_shape().as_list()[3]
+            if stride != 1 or channel_mismatch or 'pool1' in x.name:
+                # handling pool1 is to work around an architecture bug in our model
+                if stride != 1 or 'pool1' in x.name:
+                    x = AvgPooling('pool', x, stride, stride)
+                x = quan_test_L2norm('bn', x)
+                x = activate(x)
+                shortcut = Conv2D('shortcut', x, channel, 1)
+                stem = get_stem_full(x)
+            else:
+                shortcut = x
+                x = quan_test_L2norm('bn', x)
+                x = activate(x)
+                stem = get_stem_full(x)
+            return shortcut + stem
+
+        def group(x, name, channel, nr_block, stride):
+            with tf.variable_scope(name + 'blk1'):
+                x = resblock(x, channel, stride)
+            for i in range(2, nr_block + 1):
+                with tf.variable_scope(name + 'blk{}'.format(i)):
+                    x = resblock(x, channel, 1)
+            return x
+
+        with remap_variables(new_get_variable), \
+                argscope(quan_test_L2norm, momentum=0.9, eps=1e-4,train=is_training), \
+                argscope(Conv2D, use_bias=False, nl=tf.identity):
+            logits = (LinearWrap(image)
+                      # use explicit padding here, because our private training framework has
+                      # different padding mechanisms from TensorFlow
+                      .tf.pad([[0, 0], [3, 2], [3, 2], [0, 0]])
+                      .Conv2D('conv1', 64, 7, stride=2, padding='VALID', use_bias=True)
+                      .tf.pad([[0, 0], [1, 1], [1, 1], [0, 0]], 'SYMMETRIC')
+                      .MaxPooling('pool1', 3, 2, padding='VALID')
+                      .apply(group, 'conv2', 64, 2, 1)
+                      .apply(group, 'conv3', 128, 2, 2)
+                      .apply(group, 'conv4', 256, 2, 2)
+                      .apply(group, 'conv5', 512, 2, 2)
+                      .quan_test_L2norm('lastbn')
+                      .apply(nonlin)
+                      .GlobalAvgPooling('gap')
+                      #.tf.multiply(49)  # this is due to a bug in our model design
+                      .FullyConnected('fct', 10)())
+        tf.nn.softmax(logits, name='output')
+        # compute the number of failed samples
+        cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=label)
+        cost = tf.reduce_mean(cost, name='cross_entropy_loss')
+
+        wrong = tf.cast(tf.logical_not(tf.nn.in_top_k(logits, label, 1)), tf.float32, name='wrong_vector')
+        # monitor training error
+        add_moving_summary(tf.reduce_mean(wrong, name='train_error'))
+
+        # weight decay on all W of fc layers
+        #wd_w = tf.train.exponential_decay(0.0002, get_global_step_var(),480000, 0.2, True)
+        #wd_cost = tf.multiply(wd_w, regularize_cost('.*/W', tf.nn.l2_loss), name='wd_cost')
+        #add_moving_summary(cost, wd_cost)
+
+        add_param_summary(('.*/W', ['histogram']))   # monitor W
+        return tf.add_n([cost], name='cost')
+
+    def optimizer(self):
+        lr = tf.get_variable('learning_rate', initializer=0.01, trainable=False)
+        opt = tf.train.MomentumOptimizer(lr, 0.9)
+        return opt
+
+
+def get_data(train_or_test):
+    isTrain = train_or_test == 'train'
+    ds = dataset.Cifar10(train_or_test)
+    pp_mean = ds.get_per_pixel_mean(('train',))
+    if isTrain:
+        augmentors = [
+            imgaug.CenterPaste((40, 40)),
+            imgaug.RandomCrop((32, 32)),
+            imgaug.Flip(horiz=True),
+            imgaug.MapImage(lambda x: x - pp_mean),
+        ]
+    else:
+        augmentors = [
+            imgaug.MapImage(lambda x: x - pp_mean)
+        ]
+    ds = AugmentImageComponent(ds, augmentors)
+    ds = BatchData(ds, BATCH_SIZE, remainder=not isTrain)
+    if isTrain:
+        ds = MultiProcessRunner(ds, 3, 2)
+    return ds
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', help='comma separated list of GPU(s) to use.')
+
+    parser.add_argument('--dorefa',
+                        help='number of bits for W,A,G, separated by comma. Defaults to \'1,2,4\'',
+                        default='1,2,4')
+    args = parser.parse_args()
+    #NUM_UNITS = args.num_units
+
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    logger.auto_set_dir()
+
+    dataset_train = get_data('train')
+    dataset_test = get_data('test')
+
+    config = TrainConfig(
+        model=Model(),
+        dataflow=dataset_train,
+        callbacks=[
+            ModelSaver(),
+            InferenceRunner(dataset_test,
+                            [ScalarStats('cost'), ClassificationError('wrong_vector')]),
+            ScheduledHyperParamSetter('learning_rate',
+                                      [(1, 0.1), (82, 0.01), (123, 0.001), (300, 0.0002)])
+        ],
+        max_epoch=400,
+      
+    )
+    num_gpu = max(get_num_gpu(), 1)
+    launch_train_with_config(config, SyncMultiGPUTrainerParameterServer(num_gpu))
