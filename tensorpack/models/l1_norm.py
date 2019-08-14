d54da96b5873c5c652a729d742af9ce480683a72
tian <t18215595124@gmail.com>
2019-08-14 18:01:42 +0800

    Update l1_norm.py

--- tensorpack/models/l1_norm.py
+++ tensorpack/models/l1_norm.py
@@ -57,7 +57,7 @@ def L2norm_quan_train(x, train, eps=1e-05, decay=0.9, affine=True, name=None):
         
         def mean_var_with_update():
 
-            mean, variance = tf.nn.moments(quantize(nonlin(x)), [0,1,2], name='moments')
+            mean, variance = tf.nn.moments(x), [0,1,2], name='moments')
             mean = quantize(nonlin(mean))
             variance = quantize(nonlin(variance))
             with tf.control_dependencies([assign_moving_average(moving_mean, mean, decay),#计算滑动平均值
@@ -75,14 +75,9 @@ def L2norm_quan_train(x, train, eps=1e-05, decay=0.9, affine=True, name=None):
                                    initializer=tf.zeros_initializer)
             gamma = tf.get_variable('gamma', params_shape,
                                     initializer=tf.ones_initializer)
-            '''
-            x = tf.nn.batch_normalization(tf.to_bfloat16(x),tf.to_bfloat16(mean),\
-             tf.to_bfloat16(variance), tf.to_bfloat16(beta), tf.to_bfloat16(gamma), tf.to_bfloat16(eps))
-            x = tf.cast(x,dtype=tf.float32)
-            '''
 
-            x = tf.nn.batch_normalization(quantize(nonlin(x)), quantize(nonlin(mean)), \
-                quantize(nonlin(variance)), quantize(nonlin(beta)), quantize(nonlin(gamma)), eps)
+            x = tf.nn.batch_normalization(x, mean, \
+                variance, beta, gamma, eps)
 
         else:
             x = tf.nn.batch_normalization(x, quantize(nonlin(mean)), quantize(nonlin(variance)),\
