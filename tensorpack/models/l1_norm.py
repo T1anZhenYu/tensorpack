7e48e8c456816953ec3b5b168f8d66b33c851014
tian <t18215595124@gmail.com>
2019-08-16 15:57:15 +0800

    Update l1_norm.py

--- tensorpack/models/l1_norm.py
+++ tensorpack/models/l1_norm.py
@@ -23,6 +23,7 @@ __all__ = ['L2norm','L1norm','L2norm_quan_train','Lmaxnorm']
 # decay: being too close to 1 leads to slow start-up. torch use 0.9.
 # eps: torch: 1e-5. Lasagne: 1e-4
 bitG = 16
+bitg = 16
 def nonlin(x):
     return tf.clip_by_value(x, -2, 2)
 def quantize(x,k):
@@ -39,11 +40,11 @@ def quan_(x,max_value):
     assert rank is not None
     maxx = max_value
     x = x / maxx
-    n = float(2**bitG - 1)
+    n = float(2**bitg - 1)
     x = x * 0.5 + 0.5 + tf.random_uniform(
         tf.shape(x), minval=-0.5 / n, maxval=0.5 / n)
     x = tf.clip_by_value(x, 0.0, 1.0)
-    x = quantize(x, bitG) - 0.5
+    x = quantize(x, bitg) - 0.5
     return x * maxx * 2
 def quan(x):
 
@@ -58,6 +59,59 @@ def quan(x):
     x = quantize(x, bitG) - 0.5
     return x * maxx * 2
 
+
+def near_2(x):
+    def log2(x):
+        numerator = tf.log(x)
+        denominator = tf.log(tf.constant(2, dtype=numerator.dtype))
+        return numerator / denominator
+    return tf.sign(x)*tf.pow(2,tf.round(log2(tf.abs(x))))
+
+@layer_register()
+@convert_to_tflayer_args(
+    args_names=[],
+    name_mapping={
+        'use_bias': 'center',
+        'use_scale': 'scale',
+        'gamma_init': 'gamma_initializer',
+        'decay': 'momentum',
+        'use_local_stat': 'training'
+    })
+def BNN(x, train, eps=1e-05, decay=0.9, affine=True, name=None):
+
+    with tf.variable_scope(name, default_name='BatchNorm2d'):
+        params_shape = x.get_shape().as_list()
+        params_shape = params_shape[-1:]
+        moving_mean = tf.get_variable('mean', shape=params_shape,
+                                      initializer=tf.zeros_initializer,
+                                      trainable=False)
+        moving_variance = tf.get_variable('variance', shape=params_shape,
+                                          initializer=tf.ones_initializer,
+                                          trainable=False)
+        def mean_var_with_update():
+
+            mean, variance_ = tf.nn.moments(x, [0,1,2], name='moments')
+
+            
+            with tf.control_dependencies([assign_moving_average(moving_mean, mean, decay),#计算滑动平均值
+                                         assign_moving_average(moving_variance, variance, decay)]):
+                return tf.identity(mean), tf.identity(variance)
+        if train:#亲测tf.cond的第一个函数不能直接写成ture or false，所以只好用一个很蠢的方法。
+            xx = tf.constant(3)
+            yy = tf.constant(4)
+        else:
+            xx = tf.constant(4)
+            yy = tf.constant(3)
+        mean, variance = tf.cond(xx<yy, mean_var_with_update, lambda: (moving_mean, moving_variance))
+        if affine:
+            beta = tf.get_variable('beta', params_shape,
+                                   initializer=tf.zeros_initializer)
+            gamma = tf.get_variable('gamma', params_shape,
+                                    initializer=tf.ones_initializer)
+            x = tf.nn.batch_normalization(x, mean, variance, beta, gamma, eps)
+        else:
+            x = tf.nn.batch_normalization(x, mean, variance, None, None, eps)
+        return x,gamma,beta,moving_mean,moving_variance,mean,variance
 @layer_register()
 @convert_to_tflayer_args(
     args_names=[],
