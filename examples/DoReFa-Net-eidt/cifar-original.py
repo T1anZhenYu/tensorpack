c48f595d07a9c3b3e71eca662540fd9ba790992a
tian <t18215595124@gmail.com>
2019-08-19 20:41:01 +0800

    Update cifar-original.py

--- examples/DoReFa-Net-eidt/cifar-original.py
+++ examples/DoReFa-Net-eidt/cifar-original.py
@@ -57,7 +57,7 @@ class Model(ModelDesc):
         image = image / 256.0
 
         with remap_variables(binarize_weight), \
-        argscope(BatchNorm, momentum=0.9, epsilon=1e-4),\
+                argscope(BatchNorm, momentum=0.9, epsilon=1e-4), \
                 argscope(Conv2D, use_bias=False):
             logits = (LinearWrap(image)
                       .Conv2D('conv0', 48, 5, padding='VALID', use_bias=True)
@@ -65,28 +65,23 @@ class Model(ModelDesc):
                       .apply(activate)
                       # 18
                       .Conv2D('conv1', 64, 3, padding='SAME')
-                      .apply(fg)#模型的核心变动，用fg代替bn和activate
-                      .BatchNorm('bn1')
-                      .apply(activate)
+                      .apply(fg)
+                      .BatchNorm('bn1').apply(activate)
 
                       .Conv2D('conv2', 64, 3, padding='SAME')
-                      .MaxPooling('pool1', 2, padding='SAME')
-                      .apply(fg)#注意，这里要先maxpooling再做量化。
-                      #因为原来的模型maxpooling是在bn之后的
+                      .apply(fg)
                       .BatchNorm('bn2')
                       .MaxPooling('pool1', 2, padding='SAME')
                       .apply(activate)
                       # 9
                       .Conv2D('conv3', 128, 3, padding='VALID')
                       .apply(fg)
-                      .BatchNorm('bn3')
-                      .apply(activate)
+                      .BatchNorm('bn3').apply(activate)
                       # 7
 
                       .Conv2D('conv4', 128, 3, padding='SAME')
                       .apply(fg)
-                      .BatchNorm('bn4')
-                      .apply(activate)
+                      .BatchNorm('bn4').apply(activate)
 
                       .Conv2D('conv5', 128, 3, padding='VALID')
                       .apply(fg)
@@ -94,11 +89,9 @@ class Model(ModelDesc):
                       # 5
                       .Dropout(rate=0.5 if is_training else 0.0)
                       .Conv2D('conv6', 512, 5, padding='VALID')
-                      #最后一层不做量化
-                      .BatchNorm('bn6')
+                      .apply(fg).BatchNorm('bn6')
                       .apply(nonlin)
-                      .FullyConnected('fc1', 100)())
-
+                      .FullyConnected('fc1', 10)())
         tf.nn.softmax(logits, name='output')
     
         # compute the number of failed samples
