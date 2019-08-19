c3aa57f6ac32c04ec97b99efbaaa1b2bc102af0a
tian <t18215595124@gmail.com>
2019-08-19 21:25:54 +0800

    Update cifar-original.py

--- examples/DoReFa-Net-eidt/cifar-original.py
+++ examples/DoReFa-Net-eidt/cifar-original.py
@@ -142,7 +142,7 @@ def get_config():
         callbacks=[
             ModelSaver(),
             ScheduledHyperParamSetter('learning_rate',
-                                      [(60, 0.001), (120, 0.0001), (200, 0.00001)]),
+                                      [(30, 0.001), (120, 0.0001), (200, 0.00001)]),
             InferenceRunner(data_test,
                             [ScalarStats('cost'), ClassificationError('wrong-top1')]),
             #DumpTensors(['fg1/batch_mean:0','fg1/batch_var:0','fg1/realbatch_mean:0','fg1/realbatch_var:0'])
