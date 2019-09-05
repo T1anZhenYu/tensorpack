9c7afcc5cfce0648d1003eabfbb5430bd3f8de89
tian <t18215595124@gmail.com>
2019-09-05 15:01:59 +0800

    Update l1_norm.py

--- tensorpack/models/l1_norm.py
+++ tensorpack/models/l1_norm.py
@@ -140,17 +140,17 @@ def Myrangenorm(x, train, eps=1e-05, decay=0.9, affine=True, name=None):
         #c_min = tf.tile(tf.expand_dims(tf.reduce_min(x),0),params_shape)
         c_max = tf.reduce_max(x,[0,1,2])
         c_min = tf.reduce_min(x,[0,1,2])
-        # mean_, variance_ = tf.nn.moments(x, [0,1,2], name='moments')
+        mean_, variance_ = tf.nn.moments(x, [0,1,2], name='moments')
 
-        # my_bm = tf.identity((c_max+c_min)/2,name='my_bm')
-        # my_bv = tf.identity((c_max-c_min),name='my_bv')         
-        # real_bm = tf.identity(mean_,name='real_bm')
-        # real_bv = tf.identity(variance_,name='real_bv')
-        # diff_bm = tf.identity(((c_max+c_min)/2)-mean_,name='diff_bm')
-        # diff_bv = tf.identity(tf.sqrt(c_max-c_min)-tf.sqrt(variance_),name='diff_bv')
-        # ratio_bm = tf.identity((((c_max+c_min)/2))/mean_,name='ratio_bm')
-        # ratio_bv = tf.identity((c_max-c_min)/tf.sqrt(variance_),name='ratio_bv')
-        # ratio_bv2 = tf.identity(tf.sqrt(c_max-c_min)/tf.sqrt(variance_),name='ratio_bv2')
+        my_bm = tf.identity((c_max+c_min)/2,name='my_bm')
+        my_bv = tf.identity((c_max-c_min),name='my_bv')         
+        real_bm = tf.identity(mean_,name='real_bm')
+        real_bv = tf.identity(variance_,name='real_bv')
+        diff_bm = tf.identity(((c_max+c_min)/2)-mean_,name='diff_bm')
+        diff_bv = tf.identity(tf.sqrt(c_max-c_min)-tf.sqrt(variance_),name='diff_bv')
+        ratio_bm = tf.identity((((c_max+c_min)/2))/mean_,name='ratio_bm')
+        ratio_bv = tf.identity((c_max-c_min)/tf.sqrt(variance_),name='ratio_bv')
+        ratio_bv2 = tf.identity(tf.sqrt(c_max-c_min)/tf.sqrt(variance_),name='ratio_bv2')
 
 
         def mean_var_with_update():
