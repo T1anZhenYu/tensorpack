4d4da1f3a24e6c96aa7158ecca3317595589fea7
tian <t18215595124@gmail.com>
2019-09-06 12:10:15 +0800

    Update l1_norm.py

--- tensorpack/models/l1_norm.py
+++ tensorpack/models/l1_norm.py
@@ -153,12 +153,11 @@ def Myrangenorm(x, train, eps=1e-05, decay=0.9, affine=True, name=None):
         # ratio_bv = tf.identity((c_max-c_min)/tf.sqrt(variance_),name='ratio_bv')
         # ratio_bv2 = tf.identity(tf.sqrt(c_max-c_min)/tf.sqrt(variance_),name='ratio_bv2')
 
-        lambda_ = tf.get_variable('lambda_', params_shape,
-                                initializer=tf.ones_initializer)
+
         def mean_var_with_update():
 
             mean = (c_max+c_min)/2 
-            variance = (c_max - c_min)*lambda_
+            variance = tf.square(c_max - c_min)
             with tf.control_dependencies([assign_moving_average(moving_mean, mean, decay),#计算滑动平均值
                                          assign_moving_average(moving_variance, variance, decay)]):
                 return tf.identity(mean), tf.identity(variance)
