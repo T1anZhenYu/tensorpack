9890c15e4db94e7e58dbb6b8e22442c3159b2277
tian <t18215595124@gmail.com>
2019-09-07 09:26:50 +0800

    Update l1_norm.py

--- tensorpack/models/l1_norm.py
+++ tensorpack/models/l1_norm.py
@@ -154,10 +154,11 @@ def Myrangenorm(x, train, eps=1e-05, decay=0.9, affine=True, name=None):
         # ratio_bv2 = tf.identity(tf.sqrt(c_max-c_min)/tf.sqrt(variance_),name='ratio_bv2')
         lambda_ = tf.get_variable('lambda_', params_shape,
                                initializer=tf.zeros_initializer)
-
+        theta_ = tf.get_variable('theta_',params_shape,
+                                initializer=tf.zeros_initializer)
         def mean_var_with_update():
 
-            mean = (c_max+c_min)/2 
+            mean = (c_max+c_min)/2 +theta_
             variance = (c_max - c_min)*(tf.abs(1+lambda_))
             with tf.control_dependencies([assign_moving_average(moving_mean, mean, decay),#计算滑动平均值
                                          assign_moving_average(moving_variance, variance, decay)]):
